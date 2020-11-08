a2537012e89ba0c51e7a6f1dddf876314b0c6de8
pm-engr <prathmeshmahalle30@gmail.com>
2020-11-09 00:14:12 +0530

    tested best fit plne function from in test.py file

--- test.py
+++ test.py
@@ -1,5 +1,10 @@
 from algo import *
 
 dist=[[1,2,3],[4,5,6],[7,8,9]]
-cot_mat=coordinate_mat(dist_mat)
-print(cot_mat)
+co_mat=coordinate_mat(dist)
+for i in range(3):
+	for j in range(3):
+		print(co_mat[i][j])
+
+a, b, c = best_fit_plane(co_mat)
+print("a is :",a ,"b is :", b ,"c is :", c )
